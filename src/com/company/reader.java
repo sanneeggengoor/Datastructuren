0aad95786ca83702377f80546bf62d35ed4ded27
sanneeggengoor <sanneeggengoor@gmail.com>
2016-02-05 15:59:12 +0100

    reader gemaakt v1

--- src/com/company/reader.java
+++ src/com/company/reader.java
@@ -2,11 +2,23 @@ package com.company;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.*;
 
 /**
  * Created by Sanne on 5-2-2016.
  */
 public class reader {
-    BufferedReader words = new BufferedReader(new FileReader("wordlist.txt"));
-    ArrayList<String>
+
+    public void reader() {
+        BufferedReader words = new BufferedReader(new FileReader("wordlist.txt"));
+        ArrayList<String> arrayWords = new ArrayList();
+        while (true){
+            String word = words.readLine();
+            if (word == ""){
+                break;
+            }
+            arrayWords.add(word);
+        }
+        System.out.println(arrayWords);
+    }
 }
