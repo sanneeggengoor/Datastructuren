bf3d225323efe2fd1a1ffc0f92ad6616960f0ef4
sanneeggengoor <sanneeggengoor@gmail.com>
2016-02-05 16:18:14 +0100

    reader gemaakt v1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,11 @@ import java.io.FileReader;
 public class Main {
 
     public static void main(String[] args) {
+        reading();
 
+    }
+
+    private void reading(){
+        reader.reader();
     }
 }
--- src/com/company/reader.java
+++ src/com/company/reader.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.*;
 
@@ -8,16 +9,24 @@ import java.util.*;
  * Created by Sanne on 5-2-2016.
  */
 public class reader {
+    BufferedReader words;
 
     public void reader() {
-        BufferedReader words = new BufferedReader(new FileReader("wordlist.txt"));
+        try {
+            words = new BufferedReader(new FileReader("wordlist.txt"));
+        }
+        catch(Exception e){
+            System.out.println("Error");
+        }
         ArrayList<String> arrayWords = new ArrayList();
         while (true){
-            String word = words.readLine();
-            if (word == ""){
+            try {
+                String word = words.readLine();
+                arrayWords.add(word);
+            }
+            catch (Exception e) {//            if (word == ""){
                 break;
             }
-            arrayWords.add(word);
         }
         System.out.println(arrayWords);
     }
