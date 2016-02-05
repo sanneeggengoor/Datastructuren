561b44d790bc775e2770490fce19a131d17c61f6
sanneeggengoor <sanneeggengoor@gmail.com>
2016-02-05 15:51:03 +0100

    Bestand gemaakt en woordenlijst erin

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
---
+++ src/com/company/reader.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+/**
+ * Created by Sanne on 5-2-2016.
+ */
+public class reader {
+    BufferedReader words = new BufferedReader(new FileReader("wordlist.txt"));
+    ArrayList<String>
+}
