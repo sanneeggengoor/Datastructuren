9a7f61d520acd10cb70991d5667093e1c930ee40
sanneeggengoor <sanneeggengoor@gmail.com>
2016-02-12 16:30:48 +0100

    push

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,11 +5,39 @@ import java.io.FileReader;
 
 public class Main {
 
+    static String[] arrayWords;
+    static String[] arraySample;
+
     public static void main(String[] args) {
-        reader.readfile();
+  //      try {
+            arrayWords = reader.readfile("wordlist.txt");
+            arraySample = reader.readfile("sample_o]_4D=12UBt.txt");
+            int good = compareSample();
+            System.out.println(good+"/"+arraySample.length);
+    //    }
+      //  catch (Exception e){
+        //    System.out.println("Error!");
+    //    }
     }
 
-    private void samples(){
+    private static int compareSample(){
+        int count = 0;
+        for (int i = 0; i < arraySample.length; i++){
+            if (compare(arraySample[i])){
+                count++;
+            }
+        }
+        return count;
+    }
 
+    private static boolean compare(String word){
+        for (int i = 0; i < arrayWords.length; i++) {
+
+            if (word == arrayWords[i]){
+                System.out.println("Hier ben ik nu");
+                return true;
+            }
+        }
+        return false;
     }
 }
