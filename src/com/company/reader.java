68d527607576f2c5fb62464c46af26e0379263ed
sanneeggengoor <sanneeggengoor@gmail.com>
2016-02-12 15:38:34 +0100

    Hij werkt

--- src/com/company/reader.java
+++ src/com/company/reader.java
@@ -5,33 +5,34 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 
-/**
+/*
  * Created by Sanne on 5-2-2016.
  */
 public class reader {
     public static BufferedReader words;
 
-    public static void reader() {
+    public static void readfile() {
         try {
             words = new BufferedReader(new FileReader(new File("wordlist.txt")));
         }
         catch(Exception e){
-            System.out.println(e);
-            System.out.println("Error");
+            //System.out.println(e);
+            System.out.println("Error1");
         }
-        String[] arrayWords = new String[10000000];
+        String[] arrayWords = new String[1000000];
         int count = 0;
         while (true){
             try {
                 String word = words.readLine();
                 arrayWords[count]= word;
-                if (word == ""){
+                if (word == null){
                     break;
                 }
                 count++;
             }
-            catch (Exception e) {//            if (word == ""){
-                System.out.println("Error");
+            catch (Exception e) {//
+                System.out.println("Error ");
+                break;
             }
         }
         System.out.println(arrayWords.length);
