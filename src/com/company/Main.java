8160684cfc3ce8edc52e3dff1064a9194c25fc25
sanneeggengoor <sanneeggengoor@gmail.com>
2016-02-12 20:01:24 +0100

    Deel 1 af

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,53 +8,102 @@ public class Main {
 
     static String[] arrayWords;
     static String[] arraySample;
+    static String[] allsamples;
 
     public static void main(String[] args) {
-  //      try {
-            arrayWords = reader.readfile("wordlist.txt");
-            arraySample = reader.readfile("sample_o]_4D=12UBt.txt");
-            System.out.println(arraySample[999999]);
-            int good = compareSample();
-            int length = computelength(arraySample);
-            System.out.println(good+"/"+length);
-    //    }
-      //  catch (Exception e){
-        //    System.out.println("Error!");
-    //    }
+        // make an array with all the filenames of the samples
+        allsamples = reader.readfile("samplestext.txt");
+        // make an array with all the words
+        arrayWords = reader.readfile("wordlist.txt");
+        // compare the samples with the wordlist
+        compareSample();
     }
 
-    private static int compareSample(){
+    // compares samples with the wordlist
+    private static void compareSample(){
+        /* for every sample compare the words in the sample to
+         * the words in the wordlist. Starts at 1, because when
+         * starting at 0 an error occurs.
+         */
+        for (int i = 1; i < 31; i++){
+            // get the file name from place i and make array
+            String file = allsamples[i];
+            arraySample = reader.readfile(file);
+
+            // take time before comparing
+            long time1 = System.nanoTime();
+
+            // get number of words of sample that are in wordlist
+            int good = numGoodOfSample();
+
+            // take time after comparing
+            long time2 = System.nanoTime();
+            // computes total time
+            long totaltime = time2 - time1;
+
+            // find out how many words in sample
+            int length = computelength(arraySample);
+
+
+            /* prints information about which sample has how many
+             * good words and how many time it has taken.
+             */
+            System.out.println(file);
+            System.out.println("time : "+ totaltime);
+            System.out.println(good+"/"+length);
+        }
+
+
+    }
+
+    // get integer with how many words of sample are in wordlist
+    private static int numGoodOfSample(){
+        // initiate counter and get num of words in sample
         int count = 0;
         int length = computelength(arraySample);
+
+        // for every word, check if in list, if so count++
         for (int i = 0; i < length; i++){
             if (compare(arraySample[i])){
                 count++;
             }
         }
+
+        // return integer count
         return count;
     }
 
+    // boolean that checks if word is in wordlist
     private static boolean compare(String word){
+        // get num of words in wordlist
         int length = computelength(arrayWords);
-        //System.out.print(length);
-        for (int i = 0; i < length; i++) {
 
+        // for every word in list, check if equal to word
+        for (int i = 0; i < length; i++) {
+            // if so, return true
             if (word.equals(arrayWords[i])){
-                System.out.println("Hier ben ik nu");
                 return true;
             }
         }
+
+        // else return false
         return false;
     }
 
+    // get number of words in array
     private static int computelength(String[] arr){
+        // initiate counter
         int index = 0;
+
+        // while the array at index isn't null, index++
         while (true){
             if (arr[index] == null){
                 break;
             }
             index++;
         }
+
+        // return index integer
         return index;
     }
 }
--- src/com/company/reader.java
+++ src/com/company/reader.java
@@ -2,40 +2,60 @@ package com.company;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 
-/*
- * Created by Sanne on 5-2-2016.
+/* Class for reading files into arrays
+ *
  */
 public class reader {
+
+    // create bufferedreader words
     public static BufferedReader words;
 
+
+    // create an array of strings from file
     public static String[] readfile(String file) {
+        // read file in words
         try {
             words = new BufferedReader(new FileReader(new File(file)));
         }
+
+        // if it doesn't work, println with error
         catch(Exception e){
-            //System.out.println(e);
-            System.out.println("Error1");
+            System.out.println("Error1"+e);
         }
+
+        // create array of string with 1000000 places
         String[] arrayWords = new String[1000000];
+
+        // initiate counter
         int count = 0;
+
+        // while true, add word to the array at place count
         while (true){
             try {
+                // read next line to word
                 String word = words.readLine();
+                // add word to count
                 arrayWords[count]= word;
+
+                // if word is null break out while loop
                 if (word == null){
                     break;
                 }
+
+                // count + 1
                 count++;
             }
+
+            // if it doesn't work, return error
             catch (Exception e) {//
                 System.out.println("Error ");
                 break;
             }
         }
-        System.out.println(arrayWords[0]);
+
+        // return the array with the words
         return arrayWords;
     }
 }
